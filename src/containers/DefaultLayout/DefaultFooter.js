f659a425fad5fa2668adbe8642eb022b75f674cd
tiawebdemo <35865533+tiaweb@users.noreply.github.com>
2020-04-28 14:40:55 +0700

    Update DefaultFooter.js

--- src/containers/DefaultLayout/DefaultFooter.js
+++ src/containers/DefaultLayout/DefaultFooter.js
@@ -15,8 +15,8 @@ class DefaultFooter extends Component {
 
     return (
       <React.Fragment>
-        <span><a href="https://coreui.io">CoreUI</a> &copy; 2019 creativeLabs.</span>
-        <span className="ml-auto">Powered by <a href="https://coreui.io/react">CoreUI for React</a></span>
+        <span> &copy; <a href="https://tiaweb.github.io">Tiaweb</a> 2019 Surabaya.</span>
+       
       </React.Fragment>
     );
   }
